627aadaf4f6a688424fe4b0cca59b44e5bb5ec59
cesarlm05 <cesarlm05@gmail.com>
2016-12-27 19:00:55 -0300

    Actualice AMB de Productos

---
+++ PracticaTaller/PracticaTaller/Controllers/ProductosController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PracticaTaller.Models.AccesoDatos;
+using PracticaTaller.Models.LogicaNegocio;
+using PracticaTaller.ViewModels;
+
+namespace PracticaTaller.Controllers
+{
+    public class ProductosController : Controller
+    {
+        GestorProducto gestorProductos = new GestorProducto();
+        // GET: Productos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            var listaProductos = gestorProductos.Listar();
+            return View(listaProductos);
+        }
+
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        public ActionResult Guardar(ProductoViewModel producto)
+        {
+            var nuevoProducto = new Producto
+            {
+                nombre = producto.nombre,
+                descripcion = producto.descripcion,
+                stock = producto.stock,
+                precio = producto.precio                               
+            };
+
+            gestorProductos.Guardar(nuevoProducto);
+            return RedirectToAction("Listar");
+        }
+
+        public ActionResult Modificar(int id)
+        {
+            var productoModificar = gestorProductos.ObtenerPorId(id);
+            return View(productoModificar);
+        }
+
+        public ActionResult GuardarModificacion(Producto producto)
+        {
+            gestorProductos.Modificar(producto);
+            return RedirectToAction("Listar");
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            gestorProductos.Eliminar(id);
+            return RedirectToAction("Listar");
+        }
+    }
+}
---
+++ PracticaTaller/PracticaTaller/ViewModels/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticaTaller.ViewModels
+{
+    public partial class ProductoViewModel
+    {
+        public int id_producto { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public Nullable<int> stock { get; set; }
+        public Nullable<decimal> precio { get; set; }
+    }
+}
