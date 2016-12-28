83b6a2bfaf15b90f2708bf29c23c69c1ff2e9404
cesarlm05 <cesarlm05@gmail.com>
2016-12-28 09:06:46 -0300

    Actuaqlizacion de los ABM para Clientes, Productos y Proveedores

---
+++ PracticaTaller/PracticaTaller/Controllers/ProveedoresController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PracticaTaller.Models.AccesoDatos;
+using PracticaTaller.Models.LogicaNegocio;
+using PracticaTaller.ViewModels;
+
+namespace PracticaTaller.Controllers
+{
+    public class ProveedoresController : Controller
+    {
+        GestorProveedor gestorProveedor = new GestorProveedor();
+        // GET: Proveedores
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            var listaProveedores = gestorProveedor.Listar();
+            return View(listaProveedores);
+        }
+
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        public ActionResult Guardar(ProveedorViewModels proveedor)
+        {
+            var nuevoProveedor = new Proveedor
+            {
+                razon_social = proveedor.razon_social,
+                cuit = proveedor.cuit,
+                direccion = proveedor.direccion
+            };
+            gestorProveedor.Guardar(nuevoProveedor);
+            return RedirectToAction("Listar");
+        }
+
+        public ActionResult Modificar(int id)
+        {
+            var proveedorModificar = gestorProveedor.ObtenerPorId(id);
+            return View(proveedorModificar);
+        }
+
+        public ActionResult GuardarModificacion(Proveedor proveedor)
+        {
+            gestorProveedor.Modificar(proveedor);
+            return RedirectToAction("Listar");
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            gestorProveedor.Eliminar(id);
+            return RedirectToAction("Listar");
+        }
+    }
+}
---
+++ PracticaTaller/PracticaTaller/ViewModels/Proveedor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticaTaller.ViewModels
+{
+    public class ProveedorViewModels
+    {
+        public int id_proveedor { get; set; }
+        public string razon_social { get; set; }
+        public Nullable<decimal> cuit { get; set; }
+        public string direccion { get; set; }
+    }
+}
