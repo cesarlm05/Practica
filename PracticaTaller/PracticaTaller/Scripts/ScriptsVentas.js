977dbc3b31edfb14df40f9d570caf6328310297a
cesarlm05 <cesarlm05@gmail.com>
2017-02-18 19:15:11 -0300

    Agregué tabla a la vista de Ventas

--- PracticaTaller/PracticaTaller/Scripts/ScriptsVentas.js
+++ PracticaTaller/PracticaTaller/Scripts/ScriptsVentas.js
@@ -18,11 +18,61 @@
         });
 
         var cantidadVisibles = $(".producto:visible").length;
-        if (cantidadVisibles == 1) {
+        if (cantidadVisibles === 1) {
             $("#botonEnviar").attr('disabled', false);
         } else {
 
             $("#botonEnviar").attr('disabled', true);
         }
     });
+
+    $('#bt_add').click(function () {
+        agregar();
+    });
+    $('#bt_del').click(function () {
+        eliminar(id_fila_selected);
+    });
+    $('#bt_delall').click(function () {
+        eliminarTodasFilas();
+    });
 });
+
+var cont = 0;
+var id_fila_selected = [];
+function agregar() {
+    cont++;
+    var fila = '<tr class="selected" id="fila' + cont + '" onclick="seleccionar(this.id);"><td>' + cont + '</td><td>Articulo de la lista</td><td>0.00</td></tr>';
+    $('#tabla').append(fila);
+    reordenar();
+}
+
+function seleccionar(id_fila) {
+    if ($('#' + id_fila).hasClass('seleccionada')) {
+        $('#' + id_fila).removeClass('seleccionada');
+    }
+    else {
+        $('#' + id_fila).addClass('seleccionada');
+    }
+    id_fila_selected.push(id_fila);
+}
+
+function eliminar(id_fila) {
+    for (var i = 0; i < id_fila.length; i++) {
+        $('#' + id_fila[i]).remove();
+    }
+    reordenar();
+}
+
+function reordenar() {
+    var num = 1;
+    $('#tabla tbody tr').each(function () {
+        $(this).find('td').eq(0).text(num);
+        num++;
+    });
+}
+function eliminarTodasFilas() {
+    $('#tabla tbody tr').each(function () {
+        $(this).remove();
+    });
+
+}
